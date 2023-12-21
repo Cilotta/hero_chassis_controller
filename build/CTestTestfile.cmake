930dfe2c2d557a096e5fd351655713ce30604688
Cilotta <1511786107@qq.com>
2023-12-21 22:42:24 +0800

    Upload raw src to repository.

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/cilotta/hero_chassis_controller/src
+# Build directory: /home/cilotta/hero_chassis_controller/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("rm_description-1")
+subdirs("simple_chassis_controller")
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/cilotta/hero_chassis_controller/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/cilotta/hero_chassis_controller/devel;/home/cilotta/RobMaster_ws/devel;/home/cilotta/rm_ws/devel;/opt/ros/noetic'.split(';') if '/home/cilotta/hero_chassis_controller/devel;/home/cilotta/RobMaster_ws/devel;/home/cilotta/rm_ws/devel;/opt/ros/noetic' != '' else []
